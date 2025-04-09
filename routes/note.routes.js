147d76e5f291beaae4e1872dfb8fc1dd3a96c322
Ravi Panchal <ravipanchal9977@gmail.com>
2025-04-09 20:32:07 +0530

    feat(note): add getNoteByCategory controller and update routes to fetch notes by category

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -3,11 +3,13 @@ import verifyAccessToken from "../middlewares/auth.middleware.js";
 import createNote from "../controller/note/createNote.js";
 import getAllNoteOfUser from "../controller/note/getAllNotes.js";
 import updateNote from "../controller/note/updateNote.js";
+import getNoteByCategory from "../controller/note/getNoteByCategory.js";
 
 const router = express.Router();
 
 router.route("/add-note").post(verifyAccessToken, createNote);
 router.route("/get-note").get(verifyAccessToken, getAllNoteOfUser);
 router.route("/update-note").put(updateNote);
+router.route("/by-category").get(verifyAccessToken, getNoteByCategory);
 
 export default router;
